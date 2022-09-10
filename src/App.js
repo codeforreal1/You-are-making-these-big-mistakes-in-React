107273217ad4be89724cf99f84ed210d0b405297
niraj <niraj-khatiwada>
2022-09-10 19:05:54 +0545

    final code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 function App() {
+  const [user, setUser] = useState(null)
+  const history = useHistory()
+
+  React.useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/codeforreal1`, {})
+      .then((response) => {
+        setUser(response?.data)
+      })
+      .catch((error) => {
+        console.log('Error', error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>Username: {user?.name ?? ''} </>
+      <button onClick={() => history.push('/404')}>Go to a random page</button>
     </div>
-  );
+  )
 }
 
-export default App;
+function App_Solution1() {
+  const [user, setUser] = useState(null)
+  const history = useHistory()
+
+  React.useEffect(() => {
+    let isUnmounted = false
+    axios
+      .get(`https://api.github.com/users/codeforreal1`, {})
+      .then((response) => {
+        if (!isUnmounted) {
+          setUser(response?.data)
+        }
+      })
+      .catch((error) => {
+        console.log('Error', error)
+      })
+    return () => {
+      isUnmounted = true
+    }
+  }, [])
+
+  return (
+    <div>
+      <>Username: {user?.name ?? ''} </>
+      <button onClick={() => history.push('/404')}>Go to a random page</button>
+    </div>
+  )
+}
+
+function App_Solution2() {
+  const [user, setUser] = useState(null)
+  const history = useHistory()
+
+  React.useEffect(() => {
+    const abortController = new AbortController()
+    axios
+      .get(`https://api.github.com/users/codeforreal1`, {
+        signal: abortController.signal,
+      })
+      .then((response) => {
+        setUser(response?.data)
+      })
+      .catch((error) => {
+        console.log('Error', error)
+      })
+    return () => {
+      abortController.abort()
+    }
+  }, [])
+
+  return (
+    <div>
+      <>Username: {user?.name ?? ''} </>
+      <button onClick={() => history.push('/404')}>Go to a random page</button>
+    </div>
+  )
+}
+
+function App2() {
+  const [count, setCount] = useState(0)
+  const history = useHistory()
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setCount((lastCount) => lastCount + 1)
+    }, 2000)
+  }, [])
+
+  return (
+    <div>
+      <>Count: {count} </>
+      <button onClick={() => history.push('/404')}>Go to a random page</button>
+    </div>
+  )
+}
+
+function App2_Solution() {
+  const [count, setCount] = useState(0)
+  const history = useHistory()
+
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log('---')
+      setCount((lastCount) => lastCount + 1)
+    }, 2000)
+
+    return () => {
+      clearInterval(intervalId)
+    }
+  }, [])
+
+  return (
+    <div>
+      <>Count: {count} </>
+      <button onClick={() => history.push('/404')}>Go to a random page</button>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <React.Suspense fallback={null}>
+      <Switch>
+        <Route exact path="/" render={() => <App />} />
+        <Route exact path="/404" render={() => <p>Page not found</p>} />
+      </Switch>
+    </React.Suspense>
+  </BrowserRouter>
+)
